3fee84077eb653aebc8cd5996164b0ec5b6ce5cb
PedroHaugusto <pedrohaugusto12@gmail.com>
2024-04-18 11:08:24 -0300

    feat: Área de cadastro médico finalizada!

--- medico/views.py
+++ medico/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import Especialidades , DadosMedico , is_medico
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.messages import constants
 
-# Create your views here.
+def cadastro_medico(request):
+    if is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Médico já cadastrado!')
+        return redirect('/medicos/abrir_horario')
+    
+    
+    if request.method == "GET":
+        especialidades = Especialidades.objects.all()
+        return render(request, 'cadastro_medico.html', {'especialidades' : especialidades})
+    elif request.method == "POST":
+        crm = request.POST.get('crm')
+        nome = request.POST.get('nome')
+        cep = request.POST.get('cep')
+        rua = request.POST.get('rua')
+        bairro = request.POST.get('bairro')
+        numero = request.POST.get('numero')
+        cim = request.FILES('cim')
+        rg = request.FILES('rg')
+        foto = request.FILES('foto')
+        especialidade = request.POST.get('especialidade')
+        descricao = request.POST.get('descricao')
+        valor_consulta = request.POST.get('valor_consulta')
+        
+        dados_medico = DadosMedico(
+            crm=crm,
+            nome=nome,
+            cep=cep,
+            rua=rua,
+            bairro=bairro,
+            numero=numero,
+            rg=rg,
+            cedula_identidade_medica=cim,
+            foto=foto,
+            user=request.user,
+            descricao=descricao,
+            especialidade_id=especialidade,
+            valor_consulta=valor_consulta
+        )
+        
+        dados_medico.save()
+        
+        messages.add_message(request, constants.SUCESS, 'Cadastro médico realizado com sucesso!')
+        return redirect('/medicos/abrir_horario')
+        
+        
+        
